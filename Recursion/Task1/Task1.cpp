c73b9ccb9d8b542fbe6dd233a3eeb796903fbe61
alex0d <alexey.d707@gmail.com>
2022-05-02 02:27:22 +0300

    Recursion (Task1): removed std::max

--- Recursion/Task1/Task1.cpp
+++ Recursion/Task1/Task1.cpp
@@ -4,7 +4,8 @@ int recursion_max(int* array, int size, int i = 0) {
     if (i == size - 1) {
         return array[i];
     }
-    return std::max(array[i], recursion_max(array, size, i + 1));
+    int t = recursion_max(array, size, i + 1);
+    return array[i] > t ? array[i] : t;
 }
 
 int main() {
@@ -12,6 +13,7 @@ int main() {
     std::cout << "Введите размер массива: ";
     std::cin >> n;
 
+    std::cout << "Введите элементы массива: ";
     int* array = new int[n];
     for (int i = 0; i < n; i++) {
         std::cin >> array[i];
